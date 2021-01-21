321ae12c3e7c39e86fad5a72d576a4e7b49c5039
Kaspars Liepins <kasparslie@gmail.com>
2021-01-21 12:08:02 +0100

    integrated js in app

---
+++ src/Components/RecordTime.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecordTime() {
+    return(
+    <div>
+    </div>
+    )
+
+}
+  export default RecordTime;
+  
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import App from './App';
 import { mergeStyles } from '@fluentui/react';
 import reportWebVitals from './reportWebVitals';
 
